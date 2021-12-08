8fd9512db5d24b6c2378694540c45e10e4709beb
Luis Manzanero <manzanero.luis1995@gmail.com>
2021-12-09 00:43:08 +0800

    added intro section

--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,21 @@
 <template>
   <div>
     <!-- Hero Section -->
-    <div class="main-hero-container w-container">
-        <div class="div-block-2"></div>
-        <div class="div-block-3">
-          <h1 class="hero-title">
-            <span class="main-title-highlight">No Code</span> NFT Tools
-          </h1>
-          <p class="hero-subtitle">
-            A full-fledge studio to generate, customize and deploy NFTs
-          </p>
-        </div>
+    <div class="main-hero-container"> 
+        <h1 class="hero-title">
+          <span class="main-title-highlight">No Code</span> NFT Tools
+        </h1>
+        <p class="hero-subtitle">
+          A full-fledge studio to generate, customize and deploy NFTs
+        </p>
     </div> 
     <!-- Intro Section -->
-    <div class="section-2 wf-section">
-      <div class="section-container website-container w-container">
-        <h2 class="section-header-title">
+    <div class="intro-section"> 
+        <h2 class="title">
           Built for everyone to join the revolution of Digital Assets <br />
           stored in multiple decentralized networks
         </h2>
-        <p class="site-description">
+        <p class="description">
           NFT+ Studio was made to make it simple for anyone to work with NFT
           (Non-Fungible Tokens) without knowledge about coding or finding a
           developer.
@@ -27,10 +23,9 @@
         <a
           href="https://app.nftplus.studio"
           target="_blank"
-          class="primary-button early-access-btn w-button"
+          class="primary-btn"
           >Open App</a
-        >
-      </div>
+        > 
     </div> 
     <!-- Services Section -->
     <div id="our-services" class="our-service-section wf-section">
@@ -94,5 +89,78 @@
     </div>
   </div>
 </template>
+<style scoped>
+
+  .main-hero-container {
+    position: absolute;
+    text-align: center;
+    top: 20%;
+    left: 15%;
+    right: 15%; 
+  }
+
+  .main-hero-container .main-title-highlight { 
+    background-image: -webkit-gradient(linear,left top,left bottom,from(#7a05eb),to(#ff00a1));
+    background-image: linear-gradient(
+180deg,#7a05eb,#ff00a1);
+    color: #7a05eb;
+    -webkit-background-clip: text;
+    background-clip: text;
+    -webkit-text-fill-color: transparent;
+  }
+
+  .hero-title {
+    font-family: 'Titillium Web',sans-serif;
+    color: #fff;
+    font-size: 72px;
+    line-height: 109.51px;
+    text-shadow: 0 4px 10px rgb(0 0 0 / 10%);
+  }
+
+  .hero-subtitle {
+    position: relative;
+    font-family: 'Titillium Web',sans-serif;
+    color: #fff;
+    font-size: 48px;
+    line-height: 51px;
+    font-weight: 400;
+  }
+
+  .intro-section {
+    padding-top: 3em;
+    padding-bottom: 3em;
+    text-align: center;
+  }
+  
+  .intro-section .title {
+    font-size: 30px;
+    line-height: 46px;
+    text-align: center;
+  }
+
+  .intro-section .description {
+    margin-bottom: 2em;
+    font-family: 'Titillium Web',sans-serif;
+    color: rgba(0,0,0,.45);
+    font-size: 24px;
+    line-height: 36.5px;
+    text-align: center;
+  }
+
+  .primary-btn {
+    display: block;
+    padding: 0.8em 1.5em;
+    border-radius: 7px;
+    background-image: -webkit-gradient(linear,left top,left bottom,from(#ff00a1),color-stop(84%,#ff114f));
+    background-image: linear-gradient(
+180deg,#ff00a1,#ff114f 84%);
+    font-family: 'Titillium Web',sans-serif;
+    font-size: 18px;
+    font-weight: 600;
+    width: max-content;
+    height: max-content;
+    margin: auto;
+  }
+</style>
 
 <script></script>
